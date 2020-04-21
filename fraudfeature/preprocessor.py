febb6d529aaa82c0e066e2b4c2c850570dfb8f92
fancm <fancm@creditx.com>
2020-04-21 20:36:48 +0800

    update preprocessor default value

--- fraudfeature/preprocessor.py
+++ fraudfeature/preprocessor.py
@@ -5,19 +5,19 @@ from .util import similarity
 from .util import find_region, find_city, find_citytier
 
 
-def parse_normal_time(vals, missing_value=[], default=datetime(1900,1,1), **kwargs):
+def parse_normal_time(vals, default, missing_value=[],**kwargs):
     _func = np.vectorize(lambda x: parse_date(x) if parse_date(x) else default)
     out = _func(vals) if vals.shape[0] != 0 else vals.reshape(-1, 1)
     return out
 
 
-def parse_float(vals, missing_value=[], default=np.nan, **kwargs):
+def parse_float(vals, default, missing_value=[], **kwargs):
     _func = np.vectorize(lambda x: float(x) if x != "" and x is not None and x not in missing_value else default, otypes=[float])
     out = _func(vals) if vals.shape[0] != 0 else vals.reshape(-1, 1)
     return out
 
 
-def parse_str(vals, missing_value=[], default=np.nan, **kwargs):
+def parse_str(vals, default, missing_value=[], **kwargs):
     _func = np.vectorize(lambda x: str(x) if x != "" and x is not None and x not in missing_value else default)
     out = _func(vals) if vals.shape[0] != 0 else vals.reshape(-1, 1)
     return out
@@ -69,22 +69,25 @@ def parse_citytier(vals, default, missing_value=[], **kwargs):
     return out
 
 
-def parse_24month(vals, param, missing_value=[], **kwargs):
+def parse_24month(vals, default, param, missing_value=[], **kwargs):
     _M = [k for k in param.keys()]
     _M.sort()
     def _LPS(x):
         if x != "" and x is not None and x not in missing_value:
             return [x.count(v) for v in _M]
         else:
-            return [0]*len(param)
+            return [default]*len(param)
     if vals.shape[0] != 0 :
         return np.array([_LPS(v[0]) for v in vals]) 
     else:
-        return np.array([[0]*len(param)])
+        return np.array([[default]*len(param)])
 
 
-def parse_merge(vals, missing_value=[], **kwargs):
+def parse_merge(vals, default, missing_value=[], **kwargs):
     out = []
     for v in vals:
-        out.append(sum([int(_v) for _v in v if _v not in missing_value]))
+        if len(v) > 0:
+            out.append(sum([int(_v) for _v in v if _v not in missing_value]))
+        else:
+            out.append(default)
     return np.array(out)
